29dbf257e511ace109d61b80e5f0260eef523c2a
LucasWallyson <51727270+LucasWallyson@users.noreply.github.com>
2019-10-23 17:04:07 -0300

    Finish course NodeJs

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const routes = express.Router();
+
+const ProductController = require('./controllers/ProductController');
+
+routes.get("/products", ProductController.index);
+routes.get("/products/:id", ProductController.show);
+routes.delete("/products/:id", ProductController.delete);
+routes.put("/products", ProductController.store);
+routes.post("/products/:id", ProductController.update);
+
+module.exports = routes;
