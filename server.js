6f8d1c8977e2c2e57c7396d42e70939664e7368d
LucasWallyson <51727270+LucasWallyson@users.noreply.github.com>
2019-11-22 17:46:28 -0300

    .

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const requireDir = require('require-dir');
 const app = express();
 app.use(express.json());
 app.use(cors());
+
 //iniciando o DB
 mongoose.connect("mongodb://localhost:27017/nodeapi", {
     useNewUrlParser: true,
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const routes = express.Router();
 
 const ProductController = require('./controllers/ProductController');
